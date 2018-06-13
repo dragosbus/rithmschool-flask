f5f92e9872b1c183729935ab7408ee41d0fe7532
dragosbus <dragosbusuioc94@gmail.com>
2018-06-13 11:59:20 +0200

    create js file

---
+++ flask/static/js/app.js
@@ -0,0 +1,47 @@
+let url = window.location.href;
+
+function fetchPage(url) {
+    return fetch(url).then(res => res.text());
+}
+
+function createModal(page) {
+    let overlay = document.createElement('div');
+    overlay.className = 'overlay';
+
+    let modal = document.createElement('div');
+    modal.className = 'modal';
+    modal.innerHTML = page;
+
+    let closeModal = document.createElement('button');
+    closeModal.textContent = 'X';
+    closeModal.className = 'close-modal';
+
+    overlay.appendChild(modal);
+    overlay.appendChild(closeModal);
+
+    return overlay;
+}
+
+document.querySelector('.navigation').addEventListener('click', e => {
+    if (e.target.tagName === 'A') {
+        e.preventDefault();
+        let page = e.target.href;
+        let htmlText = '';
+        fetchPage(page).then(res => {
+            let fromIndex = res.indexOf('<main>');
+            let toIndex = res.indexOf('</main>');
+            htmlText = res.substring(fromIndex, toIndex);
+        }).then(() => {
+            let modal = createModal(htmlText);
+            document.body.appendChild(modal);
+            closeModal();
+        });
+    }
+});
+
+function closeModal() {
+    //close the overlay
+    document.querySelector('.overlay').addEventListener('click', e => {
+        document.querySelector('.overlay').classList.toggle('hide-modal');
+    });
+}
